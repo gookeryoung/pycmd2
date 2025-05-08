0ad9fca15d3e5638e6ec035d64770e33cd125636
gooker_young <gooker_young@qq.com>
2025-05-08 22:53:17 +0800

    更新版本: 0.1.17 → 0.1.18

--- docs/conf.py
+++ docs/conf.py
@@ -68,7 +68,7 @@ author = "gooker_young"
 # the built documents.
 #
 # The short X.Y version.
-version = "0.1.17"
+version = "0.1.18"
 # The full version, including alpha/beta/rc tags.
 release = version
 
--- src/pycmd2/__init__.py
+++ src/pycmd2/__init__.py
@@ -2,5 +2,5 @@
 
 __author__ = """gooker_young"""
 __email__ = "gooker_young@qq.com"
-__version__ = "0.1.17"
+__version__ = "0.1.18"
 __build_date__ = "2025-05-08"
