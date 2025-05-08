3d04d4c943b0b7346437e529ce8d083e1a80ac5b
gooker_young <gooker_young@qq.com>
2025-05-08 21:08:50 +0800

    +pipir

---
+++ src/pycmd2/pip/pip_install_req.py
@@ -0,0 +1,22 @@
+"""功能：pip 安装库到本地, 使用 requirements 内容"""
+
+from pycmd2.common.cli import run_cmd
+from pycmd2.common.cli import setup_client
+from pycmd2.common.consts import TRUSTED_PIP_URL
+
+cli = setup_client()
+
+
+def run_pip_install_req() -> None:
+    run_cmd([
+        "pip",
+        "install",
+        "-r",
+        "requirements.txt",
+        *TRUSTED_PIP_URL,
+    ])
+
+
+@cli.app.command()
+def main():
+    run_pip_install_req()
