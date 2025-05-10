6f7d10d4a0607dab323dba3912d7eecba42cb697
gooker_young <gooker_young@qq.com>
2025-05-10 21:17:02 +0800

    更新版本: 0.2.5 → 0.2.6

--- docs/conf.py
+++ docs/conf.py
@@ -68,7 +68,7 @@ author = "gooker_young"
 # the built documents.
 #
 # The short X.Y version.
-version = "0.2.5"
+version = "0.2.6"
 # The full version, including alpha/beta/rc tags.
 release = version
 
