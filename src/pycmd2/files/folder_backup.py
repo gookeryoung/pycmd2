f724486aa89635dce34577d1b1d3dc21c82d7421
gooker_young <gooker_young@qq.com>
2025-05-29 21:59:13 +0800

    修复folderb，只查找当前目录

--- src/pycmd2/files/folder_backup.py
+++ src/pycmd2/files/folder_backup.py
@@ -3,6 +3,7 @@
 命令: folderback.exe [DIRECTORY] --dest [DESTINATION] --max [MAX_FILE_COUNT]
 """
 
+import logging
 import os
 import pathlib
 import shutil
@@ -26,9 +27,14 @@ def zip_folder(
     """备份源文件夹 src 到目标文件夹 dst, 并删除超过 max_zip 个的备份."""
 
     timestamp = time.strftime("%Y%m%d_%H%M%S")
-    zip_files = sorted(list(dst.rglob("*.zip")), key=lambda fn: str(fn.name))
+    zip_files = sorted(list(dst.glob("*.zip")), key=lambda fn: str(fn.name))
     if len(zip_files) >= max_zip:
-        cli.run(os.remove, zip_files[: len(zip_files) - max_zip + 1])
+        remove_files = zip_files[: len(zip_files) - max_zip + 1]
+        logging.info(
+            f"超过最大备份数量 {max_zip}, "
+            f"删除旧备份: {[f.name for f in remove_files]}"
+        )
+        cli.run(os.remove, remove_files)
 
     shutil.make_archive(str(dst / f"{timestamp}_{src.name}"), "zip")
 
