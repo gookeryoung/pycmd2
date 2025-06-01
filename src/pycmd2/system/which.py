5f386ce6c0f6b700a2a8512fc3b0405f833fa3ff
gooker_young <gooker_young@qq.com>
2025-06-01 21:30:40 +0800

    增加wch模糊匹配

--- src/pycmd2/system/which.py
+++ src/pycmd2/system/which.py
@@ -5,18 +5,21 @@ import subprocess
 from typing import List
 
 from typer import Argument
+from typer import Option
+from typing_extensions import Annotated
 
 from pycmd2.common.cli import get_client
 
 cli = get_client()
 
 
-def find_executable(name: str):
+def find_executable(name: str, fuzzy: bool):
     """跨平台查找可执行文件路径"""
 
     try:
         # 根据系统选择命令
-        cmd = ["where" if cli.IS_WINDOWS else "which", name]
+        match_name = name if not fuzzy else f"*{name}*.exe"
+        cmd = ["where" if cli.IS_WINDOWS else "which", match_name]
 
         # 执行命令并捕获输出
         result = subprocess.run(
@@ -39,14 +42,15 @@ def find_executable(name: str):
 
 
 @cli.app.command()
-def main(commmands: List[str] = Argument(help="待查询命令")):  # noqa: B008
+def main(
+    commmands: Annotated[List[str], Argument(help="待查询命令")],
+    fuzzy: Annotated[
+        bool, Option("--fuzzy", "-F", help="是否模糊匹配")
+    ] = False,
+):
     for cmd in commmands:
-        path = find_executable(cmd)
+        path = find_executable(cmd, fuzzy=fuzzy)
         if path:
             logging.info(f"找到命令: [[green bold]{path}[/]]")
         else:
             logging.error(f"未找到符合的命令: [[red bold]{cmd}[/]]")
-
-
-if __name__ == "__main__":
-    main()
