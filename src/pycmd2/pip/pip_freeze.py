b07d3836982cdb6f4f98d1d09c3a6bfb4fc02082
gooker_young <gooker_young@qq.com>
2025-07-29 18:39:50 +0800

    更新pipf命令

--- src/pycmd2/pip/pip_freeze.py
+++ src/pycmd2/pip/pip_freeze.py
@@ -3,9 +3,33 @@
 命令: pipf
 """
 
+from __future__ import annotations
+
+import subprocess
+
 from pycmd2.common.cli import get_client
 
 cli = get_client()
+
+
+def check_uv_callable() -> bool | None:
+    """检查uv是否可调用.
+
+    Returns:
+        Optional[bool]: 是否可调用
+    """
+    try:
+        result = subprocess.run(
+            ["uv", "--version"],
+            capture_output=True,
+            text=True,
+            timeout=5,
+            check=False,
+        )
+    except (subprocess.TimeoutExpired, FileNotFoundError):
+        return False
+    else:
+        return result.returncode == 0
 
 
 def pip_freeze() -> None:
@@ -17,4 +41,10 @@ def pip_freeze() -> None:
 @cli.app.command()
 def main() -> None:
     """主函数."""
-    pip_freeze()
+    if check_uv_callable():
+        # 使用 uv 调用 pip freeze
+        # 这样可以避免在某些环境中 pip freeze 的输出被截断
+        cli.run_cmdstr("uv pip freeze > requirements.txt")
+    else:
+        # 直接调用 pip freeze
+        pip_freeze()
