ff5a162a94be4b204f3af7210b17cf335ec04ae5
gooker_young <gooker_young@qq.com>
2025-07-29 20:21:03 +0800

    处理 -e 问题

--- src/pycmd2/pip/pip_freeze.py
+++ src/pycmd2/pip/pip_freeze.py
@@ -36,21 +36,18 @@ def check_uv_callable() -> bool | None:
         return result.returncode == 0
 
 
-def pip_freeze() -> None:
-    """Pip freeze 命令输出库清单到当前目录下的 requirements.txt 中."""
-    options = r' | grep -v "^\-e" '
-    cli.run_cmdstr(f"pip freeze {options} > requirements.txt")
-
-
 @cli.app.command()
 def main() -> None:
     """默认调用."""
     logger.info(f"pipf {__version__}")
 
+    options = r' | grep -v "^\-e" '
+
     if check_uv_callable():
         # 使用 uv 调用 pip freeze
         # 这样可以避免在某些环境中 pip freeze 的输出被截断
-        cli.run_cmdstr("uv pip freeze > requirements.txt")
+
+        cli.run_cmdstr(f"uv pip freeze {options} > requirements.txt")
     else:
         # 直接调用 pip freeze
-        pip_freeze()
+        cli.run_cmdstr(f"pip freeze {options} > requirements.txt")
