10a1034387147694fbef367d11b4dd31d6647f56
gooker_young <gooker_young@qq.com>
2025-07-30 22:32:57 +0800

    更新gitc

--- src/pycmd2/git/git_clean.py
+++ src/pycmd2/git/git_clean.py
@@ -11,12 +11,19 @@ from typing_extensions import Annotated
 from pycmd2.common.cli import get_client
 from pycmd2.git.git_push_all import check_git_status
 
+__version__ = "0.1.1"
+__build_date__ = "2025-07-30"
+
 cli = get_client()
 logger = logging.getLogger(__name__)
 
 # 排除目录
 exclude_dirs = [
     ".venv",
+    "node_modules",
+    ".git",
+    ".idea",
+    ".vscode",
 ]
 
 
@@ -25,6 +32,8 @@ def main(
     *,
     force: Annotated[bool, Option("--force", "-f", help="强制清理")] = False,
 ) -> None:
+    logger.info(f"gitc {__version__}, 构建日期: {__build_date__}")
+
     if force:
         logger.warning("强制清理模式, 会删除未提交的修改和新文件")
 
