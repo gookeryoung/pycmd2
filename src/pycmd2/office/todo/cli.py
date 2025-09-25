094780c5e175c6c38772e3858e91572ccb5f9eba
gooker_young <gooker_young@qq.com>
2025-09-25 13:32:48 +0800

    ~cli

--- src/pycmd2/office/todo/cli.py
+++ src/pycmd2/office/todo/cli.py
@@ -11,19 +11,16 @@ cli = get_client(enable_qt=True, enable_high_dpi=True)
 
 
 def main() -> int:
-    """启动Todo应用的CLI入口点.
+    """Entry point for the Todo List Application.
 
     Returns:
-        int: 退出码
+        int: Exit code.
     """
-    # 创建Qt应用
     app = QApplication(sys.argv)
 
-    # 创建并显示Todo应用
     todo_app = TodoController()
     todo_app.show()
 
-    # 运行应用
     return app.exec_()
 
 
