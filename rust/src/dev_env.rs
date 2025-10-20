f206864789a04240eb2e6dff8381b491dde51053
gooker_young <gooker_young@qq.com>
2025-10-20 21:20:40 +0800

    +call command

--- rust/src/dev_env.rs
+++ rust/src/dev_env.rs
@@ -1,61 +1,72 @@
 use pyo3::prelude::*;
-use std::process::Command;
+use std::io::{BufRead, BufReader};
+use std::process::{Command, Stdio};
+
+fn call_command_realtime(command: &str, args: &[&str]) -> PyResult<()> {
+    let command_str = format!("{} {}", command, args.join(" "));
+    println!("正在执行命令: {}", command_str);
+
+    let mut child = Command::new(command)
+        .args(args)
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()
+        .map_err(|e| {
+            eprintln!("执行命令`{}`时出错: {}", command_str, e);
+            pyo3::exceptions::PyException::new_err("Failed to execute command")
+        })?;
+
+    // 实时输出 stdout
+    if let Some(stdout) = child.stdout.take() {
+        let reader = BufReader::new(stdout);
+        for line in reader.lines() {
+            match line {
+                Ok(line) => println!("{}", line),
+                Err(e) => eprintln!("读取 stdout 时出错: {}", e),
+            }
+        }
+    }
+
+    // 实时输出 stderr
+    if let Some(stderr) = child.stderr.take() {
+        let reader = BufReader::new(stderr);
+        for line in reader.lines() {
+            match line {
+                Ok(line) => eprintln!("{}", line),
+                Err(e) => eprintln!("读取 stderr 时出错: {}", e),
+            }
+        }
+    }
+
+    // 等待子进程结束
+    let status = child.wait().map_err(|e| {
+        eprintln!("等待子进程结束时出错: {}", e);
+        pyo3::exceptions::PyException::new_err("Failed to wait for child process")
+    })?;
+
+    if !status.success() {
+        return Err(pyo3::exceptions::PyException::new_err(
+            "Failed to install toolchain",
+        ));
+    }
+
+    println!("命令`{}`执行成功", command_str);
+    Ok(())
+}
 
 #[pyfunction]
 pub fn setup_rust_env() -> PyResult<()> {
     println!("正在设置 Rust 环境...");
 
-    // 安装指定的工具链
-    let output = Command::new("rustup")
-        .args(["toolchain", "install", "stable-x86_64-pc-windows-msvc"])
-        .output();
+    call_command_realtime(
+        "rustup",
+        ["toolchain", "install", "stable-x86_64-pc-windows-msvc"].as_ref(),
+    )?;
 
-    println!("调用命令: {:?}", output);
-
-    match output {
-        Ok(output) => {
-            if output.status.success() {
-                println!("成功安装 stable-x86_64-pc-windows-msvc 工具链");
-            } else {
-                let stderr = String::from_utf8_lossy(&output.stderr);
-                eprintln!("安装工具链时出错: {}", stderr);
-                return Err(pyo3::exceptions::PyException::new_err(
-                    "Failed to install toolchain",
-                ));
-            }
-        }
-        Err(e) => {
-            eprintln!("执行 rustup 命令时出错: {}", e);
-            return Err(pyo3::exceptions::PyException::new_err(
-                "Failed to execute rustup command",
-            ));
-        }
-    }
-
-    // 设置默认主机
-    let output = Command::new("rustup")
-        .args(["set", "default-host", "x86_64-pc-windows-msvc"])
-        .output();
-
-    match output {
-        Ok(output) => {
-            if output.status.success() {
-                println!("成功设置默认主机为 x86_64-pc-windows-msvc");
-            } else {
-                let stderr = String::from_utf8_lossy(&output.stderr);
-                eprintln!("设置默认主机时出错: {}", stderr);
-                return Err(pyo3::exceptions::PyException::new_err(
-                    "Failed to set default host",
-                ));
-            }
-        }
-        Err(e) => {
-            eprintln!("执行 rustup set 命令时出错: {}", e);
-            return Err(pyo3::exceptions::PyException::new_err(
-                "Failed to execute rustup set command",
-            ));
-        }
-    }
+    call_command_realtime(
+        "rustup",
+        ["set", "default-host", "x86_64-pc-windows-msvc"].as_ref(),
+    )?;
 
     println!("Rust 环境设置完成");
     Ok(())
