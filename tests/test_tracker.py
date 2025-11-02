95de0072016fb64963e8ec6facfafe553cfe3d88
gooker_young <gooker_young@qq.com>
2025-11-02 09:06:37 +0800

    +tracker func

---
+++ tests/test_tracker.py
@@ -0,0 +1,29 @@
+import time
+
+import pytest
+
+from pycmd2.tracker import timer
+
+
+class TestTracker:
+    """测试计时器功能."""
+
+    @pytest.mark.parametrize(
+        ("t", "expected"),
+        [(0.1, 0.1), (0.2, 0.2), (0.3, 0.3)],
+    )
+    def test_timer(
+        self,
+        t: float,
+        expected: float,
+        caplog: pytest.LogCaptureFixture,
+    ) -> None:
+        """测试计时器功能."""
+
+        @timer
+        def func(t: float) -> None:
+            time.sleep(t)
+
+        func(t)
+
+        assert f"`func` took {expected:.1f}" in caplog.text
