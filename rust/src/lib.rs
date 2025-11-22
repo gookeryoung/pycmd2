cef18a570b843962091384a1dc2dc944f9a1e724
gooker_young <gooker_young@qq.com>
2025-11-22 21:57:31 +0800

    ``` refactor(pycmd2): 移除version_info函数及相关调用，简化版本信息显示

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -7,16 +7,9 @@ mod system;
 use system::dirs::list_entries;
 use system::dirs::list_names;
 
-#[pyfunction]
-fn version_info() -> PyResult<String> {
-    Ok(format!("_pycmd2 v{}", env!("CARGO_PKG_VERSION")))
-}
-
 /// A Python module implemented in Rust.
 #[pymodule]
 fn _pycmd2(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(version_info, m)?)?;
-
     // dirs
     m.add_function(wrap_pyfunction!(list_entries, m)?)?;
     m.add_function(wrap_pyfunction!(list_names, m)?)?;
