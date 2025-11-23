f16421b7def053fb90880af5a4f93b6c56506ac2
gooker_young <gooker_young@qq.com>
2025-11-23 13:47:53 +0800

    initial commit

--- src/pycmd2/commands/dev/gittools/git_init.py
+++ src/pycmd2/commands/dev/gittools/git_init.py
@@ -6,8 +6,6 @@
 from __future__ import annotations
 
 import os
-from typing import Any
-from typing import Callable
 from typing import ClassVar
 
 from pycmd2.client import get_client
@@ -20,7 +18,7 @@ class GitInitRunner(BaseRunner):
     """GitInitRunner 类."""
 
     DESCRIPTION: str = "初始化 git 目录"
-    SUBCOMMANDS: ClassVar[list[list[str] | str | Callable[..., Any]]] = [
+    SUBCOMMANDS: ClassVar = [
         lambda: os.chdir(str(cli.cwd)),
         ["git", "init"],
         ["git", "add", "."],
