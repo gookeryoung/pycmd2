1cf74e6c1a83f9463931e00206a9b3eaa7bcdda0
gooker_young <gooker_young@qq.com>
2025-11-27 22:44:08 +0800

    -hero

--- src/pycmd2/web/apps/__init__.py
+++ src/pycmd2/web/apps/__init__.py
@@ -1,5 +1,4 @@
 __all__ = [
-    "DatabaseDemoApp",
     "DbTableDemoApp",
     "DownloaderDemoApp",
     "IconsHelpApp",
@@ -9,7 +8,6 @@ __all__ = [
     "WaveGraphApp",
 ]
 
-from .demos.database import DatabaseDemoApp
 from .demos.dbtable import DbTableDemoApp
 from .demos.downloader import DownloaderDemoApp
 from .demos.mandelbrot import MandelbrotApp
