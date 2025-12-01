7933606500088ba5b807b6b2702dd34f4e009580
gooker_young <gooker_young@qq.com>
2025-12-01 19:32:43 +0800

    ~checksum

--- src/pycmd2/commands/system/checksum/checksum.py
+++ src/pycmd2/commands/system/checksum/checksum.py
@@ -1,7 +1,10 @@
+from __future__ import annotations
+
 import hashlib
 import logging
 import sys
 from pathlib import Path
+from typing import Dict
 
 from PyQt5.QtCore import QCoreApplication
 from PyQt5.QtCore import QDir
@@ -9,6 +12,7 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QDialog
 from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtWidgets import QRadioButton
 
 from .deps.ui_checksum import Ui_ChecksumDialog
 
@@ -22,48 +26,45 @@ class ChecksumDialog(QDialog, Ui_ChecksumDialog):
         QDialog.__init__(self)
         self.setupUi(self)
 
+        self.algorithms: Dict[str, QRadioButton] = {
+            "MD5": self.m_rbMD5,
+            "SHA1": self.m_rbSHA1,
+            "SHA256": self.m_rbSHA256,
+            "SHA384": self.m_rbSHA384,
+            "SHA512": self.m_rbSHA512,
+            "Blake2b": self.m_rbBlake2b,
+            "Blake2s": self.m_rbBlake2s,
+        }
+
+        self._init_ui()
+        self.m_current_file = ""
+
+    def _init_ui(self) -> None:
+        for rb in self.algorithms.values():
+            rb.toggled.connect(self.update_checksum_method)
+
         self.m_teChecksum.setMinimumWidth(640)
-
-        self.m_rbMD5.toggled.connect(self.update_checksum_method)
-        self.m_rbSHA1.toggled.connect(self.update_checksum_method)
-        self.m_rbSHA256.toggled.connect(self.update_checksum_method)
-        self.m_rbSHA384.toggled.connect(self.update_checksum_method)
-        self.m_rbSHA512.toggled.connect(self.update_checksum_method)
-        self.m_rbBlake2b.toggled.connect(self.update_checksum_method)
-        self.m_rbBlake2s.toggled.connect(self.update_checksum_method)
-
         self.m_rbMD5.setChecked(True)
         self.m_hash_method = hashlib.md5
-        self.m_current_file = ""
 
         self.m_enable_check = False
         self.m_cbEnableCompare.setChecked(False)
-        self.m_cbEnableCompare.toggled.connect(self.enable_check)
+        self.m_cbEnableCompare.toggled.connect(self.toggle_check)
 
         self.m_pbGenerateString.clicked.connect(self.generate_string_checksum)
         self.m_pbOpenFile.clicked.connect(self.open_file)
         self.m_pbGenerateFile.clicked.connect(self.generate_file_checksum)
 
-    def enable_check(self) -> None:
+    def toggle_check(self) -> None:
         """激活比较功能."""
         self.m_enable_check = not self.m_enable_check
 
     def update_checksum_method(self) -> None:
         """更新校验和方法."""
-        if self.m_rbMD5.isChecked():
-            self.m_hash_method = hashlib.md5
-        elif self.m_rbSHA1.isChecked():
-            self.m_hash_method = hashlib.sha1
-        elif self.m_rbSHA256.isChecked():
-            self.m_hash_method = hashlib.sha256
-        elif self.m_rbSHA384.isChecked():
-            self.m_hash_method = hashlib.sha384
-        elif self.m_rbSHA512.isChecked():
-            self.m_hash_method = hashlib.sha512
-        elif self.m_rbBlake2b.isChecked():
-            self.m_hash_method = hashlib.blake2b
-        elif self.m_rbBlake2s.isChecked():
-            self.m_hash_method = hashlib.blake2s
+        for rb in self.algorithms.values():
+            if rb.isChecked():
+                self.m_hash_method = getattr(hashlib, rb.text().lower())
+                break
         else:
             logger.error("未知的校验和方法")
 
@@ -90,22 +91,22 @@ class ChecksumDialog(QDialog, Ui_ChecksumDialog):
     def open_file(self) -> None:
         """打开文件."""
         dialog = QFileDialog()
-        file_ = dialog.getOpenFileName(
+        filename, _ = dialog.getOpenFileName(
             self,
             "打开文件",
             QDir.currentPath(),
             "文件(*.*)",
         )
-        self.m_current_file: Path = Path(file_[0])
-        self.m_leFile.setText(self.m_current_file)
+        self.m_current_file: str = filename
+        self.m_leFile.setText(filename)  # type: ignore
 
     def generate_file_checksum(self) -> None:
         """生成文件校验和."""
-        if not self.m_current_file.exists():
+        if not Path(self.m_current_file).exists():
             self.m_teChecksum.setText("请输入文件")
             return
 
-        with self.m_current_file.open(encoding="utf8") as f:
+        with Path(self.m_current_file).open(encoding="utf8") as f:
             data_ = f.read()
             hash_code = self.m_hash_method(data_.encode("utf8")).hexdigest()
         if self.m_enable_check:
