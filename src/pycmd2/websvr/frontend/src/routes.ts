53ade7da5845ed549f9c74e0cb61719a054a5dfb
gooker_young <gooker_young@qq.com>
2025-12-27 19:42:42 +0800

    +rollup

--- src/pycmd2/websvr/frontend/src/routes.ts
+++ src/pycmd2/websvr/frontend/src/routes.ts
@@ -3,33 +3,36 @@ import { createMemoryHistory, createRouter } from 'vue-router'
 import HomeView from './views/HomeView.vue'
 import AboutView from './views/AboutView.vue'
 
-// Demo views - 我们将在后面创建这些组件
-const ButtonsDemo = () => import('./views/demos/ButtonsDemo.vue')
-const FormsDemo = () => import('./views/demos/FormsDemo.vue')
-const TablesDemo = () => import('./views/demos/TablesDemo.vue')
-const NotificationsDemo = () => import('./views/demos/NotificationsDemo.vue')
-const DialogsDemo = () => import('./views/demos/DialogsDemo.vue')
-const EChartsDemo = () => import('./views/demos/EChartsDemo.vue')
+// Demo views - 使用魔法注释进行代码分割分组
+const ButtonsDemo = () => import(/* webpackChunkName: "element-demos" */ './views/demos/ButtonsDemo.vue')
+const FormsDemo = () => import(/* webpackChunkName: "element-demos" */ './views/demos/FormsDemo.vue')
+const TablesDemo = () => import(/* webpackChunkName: "element-demos" */ './views/demos/TablesDemo.vue')
+const NotificationsDemo = () => import(/* webpackChunkName: "element-demos" */ './views/demos/NotificationsDemo.vue')
+const DialogsDemo = () => import(/* webpackChunkName: "element-demos" */ './views/demos/DialogsDemo.vue')
+const EChartsDemo = () => import(/* webpackChunkName: "chart-demos" */ './views/demos/EChartsDemo.vue')
 
-// Pinia Demo views
-const PiniaBasicDemo = () => import('./views/demos/PiniaBasicDemo.vue')
-const PiniaTodoDemo = () => import('./views/demos/PiniaTodoDemo.vue')
-const PiniaUserDemo = () => import('./views/demos/PiniaUserDemo.vue')
-const PiniaCompositionDemo = () => import('./views/demos/PiniaCompositionDemo.vue')
-const PiniaPersistentDemo = () => import('./views/demos/PiniaPersistentDemo.vue')
+// Pinia Demo views - 分组到 pinia-demos chunk
+const PiniaBasicDemo = () => import(/* webpackChunkName: "pinia-demos" */ './views/demos/PiniaBasicDemo.vue')
+const PiniaTodoDemo = () => import(/* webpackChunkName: "pinia-demos" */ './views/demos/PiniaTodoDemo.vue')
+const PiniaUserDemo = () => import(/* webpackChunkName: "pinia-demos" */ './views/demos/PiniaUserDemo.vue')
+const PiniaCompositionDemo = () =>
+  import(/* webpackChunkName: "pinia-demos" */ './views/demos/PiniaCompositionDemo.vue')
+const PiniaPersistentDemo = () => import(/* webpackChunkName: "pinia-demos" */ './views/demos/PiniaPersistentDemo.vue')
 
-// Todo App
-const TodoApp = () => import('./views/apps/todo/TodoApp.vue')
+// Todo App - 独立 chunk
+const TodoApp = () => import(/* webpackChunkName: "apps" */ './views/apps/todo/TodoApp.vue')
 
-// VueUse Demo views
-const UseMouseDemo = () => import('./views/vueuse/UseMouseDemo.vue')
-const UseDarkDemo = () => import('./views/vueuse/UseDarkDemo.vue')
-const UseDebounceDemo = () => import('./views/vueuse/UseDebounceDemo.vue')
-const UseThrottleDemo = () => import('./views/vueuse/UseThrottleDemo.vue')
-const UseLocalStorageDemo = () => import('./views/vueuse/UseLocalStorageDemo.vue')
-const UseClipboardDemo = () => import('./views/vueuse/UseClipboardDemo.vue')
-const UseEventListenerDemo = () => import('./views/vueuse/UseEventListenerDemo.vue')
-const UseWindowSizeDemo = () => import('./views/vueuse/UseWindowSizeDemo.vue')
+// VueUse Demo views - 分组到 vueuse-demos chunk
+const UseMouseDemo = () => import(/* webpackChunkName: "vueuse-demos" */ './views/vueuse/UseMouseDemo.vue')
+const UseDarkDemo = () => import(/* webpackChunkName: "vueuse-demos" */ './views/vueuse/UseDarkDemo.vue')
+const UseDebounceDemo = () => import(/* webpackChunkName: "vueuse-demos" */ './views/vueuse/UseDebounceDemo.vue')
+const UseThrottleDemo = () => import(/* webpackChunkName: "vueuse-demos" */ './views/vueuse/UseThrottleDemo.vue')
+const UseLocalStorageDemo = () =>
+  import(/* webpackChunkName: "vueuse-demos" */ './views/vueuse/UseLocalStorageDemo.vue')
+const UseClipboardDemo = () => import(/* webpackChunkName: "vueuse-demos" */ './views/vueuse/UseClipboardDemo.vue')
+const UseEventListenerDemo = () =>
+  import(/* webpackChunkName: "vueuse-demos" */ './views/vueuse/UseEventListenerDemo.vue')
+const UseWindowSizeDemo = () => import(/* webpackChunkName: "vueuse-demos" */ './views/vueuse/UseWindowSizeDemo.vue')
 
 const routes = [
   { path: '/', component: HomeView },
--- src/pycmd2/websvr/frontend/vite.config.ts
+++ src/pycmd2/websvr/frontend/vite.config.ts
@@ -1,28 +1,113 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src')
-    }
-  },
-  build: {
-    // 设置输出目录为 output
-    outDir: 'output'
-  },
-  server: {
-    host: '0.0.0.0', // 允许从任何IP地址访问
-    port: 5173,
-    // 启用 CORS 以便 WebView 可以访问
-    cors: true,
-    // 监听文件变化
-    watch: {
-      usePolling: true,
-      interval: 100
+export default defineConfig(({ command, mode }) => {
+  const isAnalyze = mode === 'analyze'
+
+  const baseConfig: any = {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    build: {
+      // 设置输出目录为 output
+      outDir: 'output',
+      // 优化 chunk 分割
+      rollupOptions: {
+        output: {
+          // 手动分割第三方库
+          manualChunks: id => {
+            // 第三方库分组策略
+            if (id.includes('node_modules')) {
+              // Vue 生态系统
+              if (id.includes('vue') || id.includes('vue-router') || id.includes('pinia')) {
+                return 'vue-vendor'
+              }
+
+              // Element Plus UI 库
+              if (id.includes('element-plus') || id.includes('@element-plus')) {
+                return 'element-plus'
+              }
+
+              // 图表库
+              if (id.includes('echarts') || id.includes('vue-echarts')) {
+                return 'charts'
+              }
+
+              // VueUse 工具库
+              if (id.includes('@vueuse')) {
+                return 'vueuse'
+              }
+
+              // 其他工具库
+              if (id.includes('lodash') || id.includes('dayjs') || id.includes('axios')) {
+                return 'utils'
+              }
+
+              // 其他第三方库
+              return 'vendor'
+            }
+          },
+          // 优化 chunk 命名
+          chunkFileNames: chunkInfo => {
+            const facadeModuleId = chunkInfo.facadeModuleId
+            if (facadeModuleId) {
+              const fileName = facadeModuleId.split('/').pop() || 'chunk'
+              return `js/${fileName}-[hash].js`
+            }
+            return 'js/[name]-[hash].js'
+          },
+          // 静态资源命名
+          assetFileNames: assetInfo => {
+            const info = assetInfo.name?.split('.') || []
+            const extType = info[info.length - 1] || ''
+            if (/\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/i.test(assetInfo.name || '')) {
+              return `media/[name]-[hash][extname]`
+            }
+            if (/\.(png|jpe?g|gif|svg)(\?.*)?$/i.test(assetInfo.name || '')) {
+              return `images/[name]-[hash][extname]`
+            }
+            if (/\.(woff2?|eot|ttf|otf)(\?.*)?$/i.test(assetInfo.name || '')) {
+              return `fonts/[name]-[hash][extname]`
+            }
+            return `assets/[name]-[hash][extname]`
+          }
+        }
+      },
+      // 设置 chunk 大小警告限制
+      chunkSizeWarningLimit: 1000,
+      // 启用 CSS 代码分割
+      cssCodeSplit: true
+    },
+    server: {
+      host: '0.0.0.0', // 允许从任何IP地址访问
+      port: 5173,
+      // 启用 CORS 以便 WebView 可以访问
+      cors: true,
+      // 监听文件变化
+      watch: {
+        usePolling: true,
+        interval: 100
+      }
     }
   }
+
+  // 如果是分析模式，添加打包分析插件
+  if (isAnalyze && baseConfig.plugins) {
+    // 安装: npm install --save-dev rollup-plugin-visualizer
+    // 然后取消下面注释:
+    // import { visualizer } from 'rollup-plugin-visualizer'
+    // baseConfig.plugins.push(visualizer({
+    //   filename: 'output/stats.html',
+    //   open: true,
+    //   gzipSize: true,
+    //   brotliSize: true
+    // }))
+  }
+
+  return baseConfig
 })
